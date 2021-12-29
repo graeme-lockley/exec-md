98dd4ecc410a3c67ef4f50586f53fb574065e0f4
Graeme Lockley <graeme.lockley@gmail.com>
2021-12-29 10:30:55 +0200

    chore: replaced tslint with eslint

--- core/load-resource/src/index.ts
+++ core/load-resource/src/index.ts
@@ -1,23 +1,23 @@
-import { AbstractFile } from "@observablehq/stdlib";
+import { AbstractFile } from '@observablehq/stdlib'
 
 export const load = async (name: string) => {
-    const fetchResponse = await fetch(name);
-    const text = await fetchResponse.text();
+  const fetchResponse = await fetch(name)
+  const text = await fetchResponse.text()
 
-    return text;
-};
+  return text
+}
 
 class FA extends AbstractFile {
-    name: string;
+  name: string
 
-    constructor(name: string) {
-        super(name, name);
-    }
+  constructor (name: string) {
+    super(name, name)
+  }
 
-    url() {
-        return this.name;
-    }
+  url () {
+    return this.name
+  }
 }
 
 export const loadSource = (url: string): FA =>
-    new FA(url);
+  new FA(url)
