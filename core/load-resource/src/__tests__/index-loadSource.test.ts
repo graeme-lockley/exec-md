1f5b43c6cbad01c473f9f36ca4832e8d92abc17e
Graeme Lockley <graeme.lockley@gmail.com>
2021-12-29 15:14:54 +0000

    test: add tests for both loadSource and load

--- core/load-resource/src/__tests__/index-loadSource.test.ts
+++ core/load-resource/src/__tests__/index-loadSource.test.ts
@@ -1,8 +1,48 @@
-import { loadSource } from '../index'
+import { load, loadSource } from '../index'
 
-test('bob', () => {
-  const url = 'https://graeme-lockley.github.io/notebook-viewer-2/penguins.csv'
-  const content = loadSource(url)
+describe('loadSource', () => {
+  test('valid URL', async () => {
+    globalThis.fetch = validFetch
 
-  expect(content.name).toEqual(url)
+    const csv = await loadAsText('arb-url')
+
+    expect(csv).toEqual('Hello world')
+  })
+
+  test('invalid URL', async () => {
+    globalThis.fetch = invalidFetch
+
+    await expect(() => loadAsText('arb-url')).rejects.toThrow('Invalid URL')
+  })
 })
+
+describe('load', () => {
+  test('valid URL', async () => {
+    globalThis.fetch = validFetch
+
+    const text = await load('arb-url')
+    expect(text).toEqual('Hello world')
+  })
+
+  test('invalid URL', async () => {
+    globalThis.fetch = invalidFetch
+
+    await expect(() => load('arb-url')).rejects.toThrow('Invalid URL')
+  })
+})
+
+const validFetch = (url: string): Promise<any> => {
+  return Promise.resolve({
+    ok: true,
+    text: () => Promise.resolve('Hello world')
+  })
+}
+
+const invalidFetch = (url: string): Promise<any> => {
+  return Promise.reject(new Error('Invalid URL'))
+}
+
+const loadAsText = async (url: string): Promise<any> => {
+  const c = loadSource(url) as any
+  return await c.text()
+}
