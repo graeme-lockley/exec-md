a942beac7feb07c470a1ba6cead36728d16cc198
Graeme Lockley <graeme.lockley@gmail.com>
2022-01-22 08:28:56 +0200

    feature: add a release script

---
+++ scripts/release.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+(
+    cd core/load-resource
+    npm publish
+)
+
+(
+    cd core/javascript-parser
+    npm publish
+)
+
+(
+    cd core/runtime
+    npm publish
+)
+
+(
+    cd core/plugin-common
+    npm publish
+)
+
+(
+    cd core/plugin-javascript-x
+    npm publish
+)
+
+(
+    cd core/plugin-javascript-x-assert
+    npm publish
+)
+
+(
+    cd core/plugin-javascript-x-inline
+    npm publish
+)
+
+(
+    cd core/plugin-javascript-x-view
+    npm publish
+)
+
+(
+    cd core/plugin-kroki-x
+    npm publish
+)
+
+(
+    cd core/core
+    npm publish
+)
+
