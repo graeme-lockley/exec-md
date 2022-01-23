19e2ba5c08b3d644a28b4e2b11dde54780fbc2cb
Graeme Lockley <graeme.lockley@gmail.com>
2022-01-23 09:37:51 +0000

    doc: add recognition note

--- scripts/check-dependencies.sh
+++ scripts/check-dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# grabbed the following code from a post by https://github.com/ujwal-setlur
+# so credit to Ujwal - complaints to me
+
 packages=$(rush list -p --json | jq -r '. | .projects[] | .name + " " + .path + "/"')
 
 echo "${packages}" | awk '{print $2 "package.json"}' | xargs -n 1 -x ncu "${@}" --packageFile
