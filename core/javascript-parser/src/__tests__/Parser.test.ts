a7d3b9d6faeac9b8bad294d56bd6dd50fa6e0d75
Graeme Lockley <graeme.lockley@gmail.com>
2022-01-23 10:28:36 +0000

    test: add tests for all used scenarios

--- core/javascript-parser/src/__tests__/Parser.test.ts
+++ core/javascript-parser/src/__tests__/Parser.test.ts
@@ -1,11 +1,50 @@
-import { parse } from '../Parser'
+import { parse, type AssignmentStatement, type ExceptionStatement, type ImportStatement } from '../index'
 
 describe('Parser', () => {
-  test('Expression', () => {
-    const ast: any = parse('1 + 2')
+  test('Simple expression', () => {
+    const ast = parse('1 + 2') as AssignmentStatement
 
-    expect(ast.dependencies).toEqual([])
-    expect(ast.name).toBeUndefined()
     expect(ast.type).toEqual('assignment')
+    expect(ast.name).toBeUndefined()
+    expect(ast.dependencies).toEqual([])
+    expect(ast.body).toEqual('1 + 2')
+  })
+
+  describe('Assignment', () => {
+    test('Simple', () => {
+      const ast = parse('x = y + 2') as AssignmentStatement
+
+      expect(ast.type).toEqual('assignment')
+      expect(ast.name).toEqual('x')
+      expect(ast.dependencies).toEqual(['y'])
+      expect(ast.body).toEqual('y + 2')
+    })
+
+    test('Block', () => {
+      const ast = parse('x = { const z = y + 2; return z + 1; }') as AssignmentStatement
+
+      expect(ast.type).toEqual('assignment')
+      expect(ast.name).toEqual('x')
+      expect(ast.dependencies).toEqual(['y'])
+      expect(ast.body).toEqual('{ const z = y + 2; return z + 1; }')
+    })
+  })
+
+  test('Import', () => {
+    const ast = parse('import { a as a1, b } from \'https://hello.world/bob.md\'') as ImportStatement
+
+    expect(ast.type).toEqual('import')
+    expect(ast.names).toEqual([
+      { alias: 'a1', name: 'a' },
+      { alias: 'b', name: 'b' }
+    ])
+    expect(ast.urn).toEqual('https://hello.world/bob.md')
+  })
+
+  test('Error', () => {
+    const ast = parse('import { a as a1 b } from \'https://hello.world/bob.md\'') as ExceptionStatement
+
+    expect(ast.type).toEqual('exception')
+    expect(ast.exception).toBeTruthy()
   })
 })
