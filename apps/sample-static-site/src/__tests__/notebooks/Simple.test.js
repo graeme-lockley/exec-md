85cdb055991b244ebaf5e5908363e643740d5539
Graeme Lockley <graeme.lockley@gmail.com>
2022-01-29 12:04:29 +0000

    test: confirming that an selected value is undefined

--- apps/sample-static-site/src/__tests__/notebooks/Simple.test.js
+++ apps/sample-static-site/src/__tests__/notebooks/Simple.test.js
@@ -15,7 +15,7 @@ beforeAll(() => {
 beforeEach(async () => {
     runtime = createRuntime()
 
-    const imports = importModulePrime(runtime, './public/notebooks/simple.md')
+    const imports = importModule(runtime, './public/notebooks/simple.md')
     module = imports[0]
     await Promise.all(imports[1])
 })
@@ -25,42 +25,50 @@ afterEach(() => {
     runtime = undefined
 })
 
-test('value', async () => {
-    const value = await module.value('value')
-    const valuesSquared = await module.value('valuesSquared')
+describe('Simple Notebook', () => {
+    test('value', async () => {
+        const value = await module.value('value')
+        const valuesSquared = await module.value('valuesSquared')
 
-    expect(value).toEqual([2, 4, 6, 8, 10])
-    expect(valuesSquared).toEqual([4, 16, 36, 64, 100])
-})
-
-test('athletes', async () => {
-    const athletes = await module.value('athletes')
-
-    expect(athletes[0]).toEqual({
-        "bronze": 0,
-        "date_of_birth": new Date('1969-10-17T00:00:00.000Z'),
-        "gold": 0,
-        "height": 1.72,
-        "id": 736041664,
-        "info": null,
-        "name": "A Jesus Garcia",
-        "nationality": "ESP",
-        "sex": "male",
-        "silver": 0,
-        "sport": "athletics",
-        "weight": 64,
+        expect(value).toEqual([2, 4, 6, 8, 10])
+        expect(valuesSquared).toEqual([4, 16, 36, 64, 100])
     })
 
-    expect(athletes.length).toEqual(11538)
+    test('athletes', async () => {
+        const athletes = await module.value('athletes')
+
+        expect(athletes[0]).toEqual({
+            "bronze": 0,
+            "date_of_birth": new Date('1969-10-17T00:00:00.000Z'),
+            "gold": 0,
+            "height": 1.72,
+            "id": 736041664,
+            "info": null,
+            "name": "A Jesus Garcia",
+            "nationality": "ESP",
+            "sex": "male",
+            "silver": 0,
+            "sport": "athletics",
+            "weight": 64,
+        })
+
+        expect(athletes.length).toEqual(11538)
+    })
+
+    test('arbList', async () => {
+        const arbList = await module.value('arbList')
+
+        expect(arbList.length).toEqual(20)
+    })
+
+    test('exponent', async () => {
+        const exponent = await module.value('exponent')
+
+        expect(exponent).toEqual(undefined)
+    })
 })
 
-test('arbList', async () => {
-    const arbList = await module.value('arbList')
-
-    expect(arbList.length).toEqual(20)
-})
-
-const importModulePrime = (runtime, filename) => {
+const importModule = (runtime, filename) => {
     const module = runtime.module();
     const modules = []
 
